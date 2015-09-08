7e320d208672d51b7d6da1067c11a8ed918c170d
Sause <lazureys@gmail.com>
2015-09-08 19:42:10 +0800

    Picture sharing

--- src/pages/PicShare.react.js
+++ src/pages/PicShare.react.js
@@ -32,8 +32,44 @@ var PicShare = React.createClass({
           <div className="pic-share__pic">
             <img src="assets/images/example.jpg" alt="image" />
           </div>
+          <div className="pic-share__share pure-g">
+            <div className="pure-u-4-24">啵</div>
+            <div className="pure-u-16-24">
+              <ul>
+                <li>
+                  <img src="assets/images/example.jpg" alt="userA" />
+                </li>
+              </ul>
+            </div>
+            <div className="pure-u-4-24">3</div>
+          </div>
           <div className="pic-share__comment">
-          Comment
+            <ul>
+              <li className="pic-share__comment-item">
+                <p>
+                  <span className="comment__user">村口王师傅</span>:
+                  <span className="comment__content">你好，烫头五块钱。</span>
+                </p>
+              </li>
+              <li className="pic-share__comment-item">
+                <p>
+                  <span className="comment__user">村口王师傅</span>:
+                  <span className="comment__content">你好，烫头五块钱。</span>
+                </p>
+              </li>
+              <li className="pic-share__comment-item">
+                <p>
+                  <span className="comment__user">村口王师傅</span>:
+                  <span className="comment__content">你好，烫头五块钱。</span>
+                </p>
+              </li>
+              <li className="pic-share__comment-item">
+                <p>
+                  <span className="comment__user">村口王师傅</span>:
+                  <span className="comment__content">你好，烫头五块钱。</span>
+                </p>
+              </li>
+            </ul>
           </div>
           <div className="hot-pic">
             <h2>热门</h2>
