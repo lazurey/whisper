607b83f46440935dd759ef57de450bfc16d876c4
Sause <lazureys@gmail.com>
2015-10-13 21:43:58 +0800

    timeline infinite loading

--- src/pages/Timeline.react.js
+++ src/pages/Timeline.react.js
@@ -7,7 +7,8 @@ var React = require('react'),
     Swipeable = require('react-swipeable'),
     DocumentTitle = require('react-document-title');
 
-var SWIPE_AT_LEAST = 20;
+var SWIPE_AT_LEAST = 20,
+    PAGE_COUNT = 9;
 
 var Timeline = React.createClass({
   statics: {
@@ -15,18 +16,22 @@ var Timeline = React.createClass({
   },
 
   _load_user: function (uid) {
-    api.user_data({id: uid, type: 2, page: 1, size: 9}).then(function(response) {
+    api.user_data({id: uid, type: 2, page: this.state.current_page, size: PAGE_COUNT}).then(function(response) {
       if (this.isMounted()) {
         if (!response) return;
         
         var userData = response.objects.data;
         var this_title = userData.Nickname + "的时间线 | 粑粑麻麻，别让我输在起跑线上哦--爱你的宝 发自KIZZ APP";
+        var cur_piclist = this.state.piclist || [];
+
+        var cur_page = this.state.current_page;
 
         this.setState({
           title: this_title,
           avatar: userData.Avatar,
+          current_page: cur_page + 1,
           nickname: userData.Nickname,
-          piclist: userData.PicList
+          piclist: cur_piclist.concat(userData.PicList)
         });
 
       }
@@ -44,6 +49,7 @@ var Timeline = React.createClass({
       threshold: 60,
       timeline_style: {right: 'auto'},
       currentIndex: 0,
+      current_page: 1,
       title: "粑粑麻麻，别让我输在起跑线上哦--爱你的宝 发自KIZZ APP",
       avatar: "",
       nickname: "",
@@ -76,6 +82,11 @@ var Timeline = React.createClass({
         currentIndex: cur_index - 1,
         timeline_style: {right: 'auto'}
       });
+    }
+    if (this.state.currentIndex >= (this.state.current_page - 1) * PAGE_COUNT - 3) {
+      var cur_page = this.state.current_page;
+      var uid = this.props.params.uid || 15;
+      this._load_user(uid);
     }
   },
 
