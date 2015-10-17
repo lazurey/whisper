5bea6902a55433fd5375e62bfd4df9175f314e84
Sause <lazureys@gmail.com>
2015-10-17 21:07:25 +0800

    tag left and right

--- src/components/Tags.react.js
+++ src/components/Tags.react.js
@@ -18,8 +18,10 @@ var Tags = React.createClass({
                   .uniq()
                   .map(function(tag) {
                     if (!tag) return;
+                    var tag_style = "pic-share__tag-item ";
+                    tag_style += (tag.P == 1) ? " tag--left" : " tag--right";
                     var pos = {"left": tag.X, "top": tag.Y};
-                    return <div className="pic-share__tag-item" style={pos}>
+                    return <div className={tag_style} style={pos}>
                               <div className="tag-icon--container">
                                 <div className="tag-icon">
                                   <div className="tag-icon--bg"></div>
--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -1,21 +1,21 @@
-// import gulp from 'gulp'
-// import gutil from 'gulp-util'
-// import imagemin from 'gulp-imagemin'
-// import pngquant from 'imagemin-pngquant'
-// import imageminGifsicle from 'imagemin-gifsicle'
+import gulp from 'gulp'
+import gutil from 'gulp-util'
+import imagemin from 'gulp-imagemin'
+import pngquant from 'imagemin-pngquant'
+import imageminGifsicle from 'imagemin-gifsicle'
 
-// const TASK_NAME = 'imagemin'
+const TASK_NAME = 'imagemin'
 
-// function imageminTask() {
-//   return gulp.autoRegister(TASK_NAME, (config)=> {
-//     gulp.src(config.src)
-//         .pipe(imagemin({
-//             progressive: true,
-//             svgoPlugins: [{removeViewBox: false}],
-//             use: [pngquant(), imageminGifsicle({interlaced: false})]
-//         }))
-//         .pipe(gulp.dest(config.dest));
-//   })
-// }
+function imageminTask() {
+  return gulp.autoRegister(TASK_NAME, (config)=> {
+    gulp.src(config.src)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant(), imageminGifsicle({interlaced: false})]
+        }))
+        .pipe(gulp.dest(config.dest));
+  })
+}
 
-// export default gulp.task(TASK_NAME, imageminTask)
+export default gulp.task(TASK_NAME, imageminTask)
